b857b3b240fbb58c3a8e34af750eccd33fc5ebbd
laila <chokri.laila.ch@gmail.com>
2023-07-18 17:24:50 +0100

    backing up

--- webservParsing/main.cpp
+++ webservParsing/main.cpp
@@ -1,9 +1,28 @@
 #include "conf.hpp"
 #include "parsing.hpp"
+#include <algorithm>
 
-void servers_count(int &i, int &j)
+void servers_count(int &i, int &j, std::string path)
 {
-  :wq
+  std::string buff;
+  std::ifstream file(path);
+  std::string word;
+
+  while (getline(file, buff))
+  {
+//      std::cout << buff  << "    is bf" << std::endl;
+    std::stringstream  buffer(buff);
+     buffer.erase(0, buffer.find_first_not_of('\t'));
+      getline(buffer, word, ' ');
+      word.erase(std::remove_if(word.begin(), word.end(), isspace), word.end());
+      std::cout << word << "    is word" << std::endl;
+      if(!word.compare("server"))
+        i++;
+      else if(!word.compare("location"))
+        j++;
+  }
+
+  std::cout << "total servrers: " << i << " and tot locations is : " << j << std::endl;
 
 }
 
@@ -26,7 +45,8 @@ int main(int ac, char **av)
 
   int i = 0;
   int j = 0;
-  servers_count(&i, &j, path);
+  servers_count(i, j, path);
 
+  std::cout << "total servrers: " << i << " and tot locations is : " << j << std::endl;
 }
 
--- webservParsing/parsing.hpp
+++ webservParsing/parsing.hpp
@@ -1,20 +1,49 @@
+#ifndef PARSING_HPP
+#define  PARSING_HPP
+
+#include <string>
+#include <cstdlib>
 #include <iostream>
+
+
+
+
+#include <stack>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <string>
+#include  <cctype> 
+
+
 
 
 class locations
 {
-	std::string root;
-  std::string pattern;
-	std::string err_page;
-  int client_max_body_size;
+  private:
+
+  public:
+  	std::string root;
+    std::string pattern;
+  	std::string err_page;
+    int client_max_body_size;
+    locations();
+    ~locations();
 };
 
 class servers
 {
+  private:
+
+  public:
 	locations *location;
   std::string  port;
   std::string   host;
   std::string   server_name;
   int client_max_body_size;
-
+  servers();
+  ~servers();
 };
+
+#endif
