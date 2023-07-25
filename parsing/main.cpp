57d3504dde4d613b3e3d0e0871062f4d869a3f20
laila-chk <chokri.laila.ch@gmail.com>
2023-07-25 19:50:00 +0100

    we are storing all directives, they still need err cheking

--- parsing/main.cpp
+++ parsing/main.cpp
@@ -1,167 +1,170 @@
 #include "conf.hpp"
 #include "parsing.hpp"
 
-void ft_perr(std::string msg)
-{
-	std::cout << msg << std::endl;
-	exit(1);
+void ft_perr(std::string msg) {
+  std::cout << msg << std::endl;
+  exit(1);
 }
 
-bool directive(std::string buff, std::vector<std::string> serv_dirs, servers &srv, bool inLoc, int ii)
-{
-	if (!buff.compare("{"))
-		return true;
-	int j = 0;
+bool directive(std::string buff, std::vector<std::string> serv_dirs,
+               servers &srv, bool inLoc, int ii) {
+  if (!buff.compare("{"))
+    return true;
+  int j = 0;
 
-	std::vector<std::string> words;
-	for(size_t i = 0; i < buff.length(); i++)
-	{
-		if (buff[i] == ' ' || buff[i] == '\t' || buff[i] == ';' || buff[i] == ':')
-		{
-			words.push_back(buff.substr(j, i - j));
-			while (buff[i] == ' ' || buff[i] == '\t')
-				i++;
-			j = i;
-		}
-	}
+  std::vector<std::string> words;
+  for (size_t i = 0; i < buff.length(); i++) {
+    if (buff[i] == ' ' || buff[i] == '\t' || buff[i] == ';' || buff[i] == ':') {
+      words.push_back(buff.substr(j, i - j));
+      while (buff[i] == ' ' || buff[i] == '\t')
+        i++;
+      j = i;
+    }
+  }
 
-	for (int i = 0; i < 10; i++)
-	{
-		//std::cout << words[0]<< std::endl;
-		if (!inLoc && !words[0].compare(serv_dirs[i]))
-		{
-			if (!words[0].compare("server_name"))
-				srv.server_name = words[1];
-			else if (!words[0].compare("listen"))
-				srv.port = words[1];
-			else if (!words[0].compare("client_max_body_size"))
-				srv.client_max_body_size = words[1];
-			return true;
-		}
-    else if(inLoc && !words[0].compare(serv_dirs[i]))
-    {
-			if (!words[0].compare("pattern"))
-				srv.loc[ii].pattern = words[1];
-			else if (!words[0].compare("methods"))
-				srv.loc[ii].methods = words[1];
-			else if (!words[0].compare("redir_path"))
-				srv.loc[ii].redir_path = words[1];
-			else if (!words[0].compare("root"))
-				srv.loc[ii].root = words[1];
-			else if (!words[0].compare("autoindex"))
-				srv.loc[ii].autoindex = true ;
+  for (int i = 0; i < 10; i++) {
+    // std::cout << words[0]<< std::endl;
+    if (!inLoc && !words[0].compare(serv_dirs[i])) {
+      if (!words[0].compare("server_name"))
+        srv.server_name = words[1];
+      else if (!words[0].compare("listen"))
+        srv.port = words[1];
+      else if (!words[0].compare("client_max_body_size"))
+        srv.client_max_body_size = words[1];
+      return true;
+    } else if (inLoc && !words[0].compare(serv_dirs[i])) {
+      if (!words[0].compare("pattern"))
+        srv.loc[ii].pattern = words[1];
+
+      else if (!words[0].compare("limit_except"))
+        srv.loc[ii].methods = words[1];
+
+      else if (!words[0].compare("return"))
+        srv.loc[ii].redir_path = words[1];
+
+      else if (!words[0].compare("root"))
+        srv.loc[ii].root = words[1];
+      else if (!words[0].compare("autoindex"))
+        srv.loc[ii].autoindex = true;
+      else if (!words[0].compare("index"))
+        srv.loc[ii].def.push_back(words[1]);
+
+      else if (!words[0].compare("cgi")) {
+        if (words.size() != 3)
+          ft_perr("Error: cgi bad format!");
+        srv.loc[ii].cgi.push_back(make_pair(words[1], words[2]));
+      } // needs protection and like
+        // many other attrs, we need to check wether
+        // we have the right amount of words aka
+        // words.length() is right for each directive.
+
       return true;
     }
-	}
+  }
   std::cout << buff << std::endl;
-	return false;
+  return false;
 }
 
-void  dirs(std::vector<std::string> &serv_dirs)
-{
-	// we cann later add location directives too, since they will be know by their indexs.
-	serv_dirs.push_back("server_name");
-	serv_dirs.push_back("listen");
-	serv_dirs.push_back("client_max_body_size");
+void dirs(std::vector<std::string> &serv_dirs) {
+  // we cann later add location directives too, since they will be know by their
+  // indexs.
+  serv_dirs.push_back("server_name");
+  serv_dirs.push_back("listen");
+  serv_dirs.push_back("client_max_body_size");
 
-  //location directives:
+  // location directives:
   serv_dirs.push_back("pattern");
-  serv_dirs.push_back("methods");
-  serv_dirs.push_back("redir_path");
+  serv_dirs.push_back("limit_except");
+  serv_dirs.push_back("return");
   serv_dirs.push_back("root");
   serv_dirs.push_back("autoindex");
-	serv_dirs.push_back("error_page");
+  serv_dirs.push_back("index");
   serv_dirs.push_back("cgi");
 }
 
-void contexts_count(std::vector<servers> &srvs, std::string path)
-{
-	std::string buff;
-	std::ifstream file(path);
-	std::string word;
-	std::vector<std::string> serv_dirs;
-	int   i = 0;
-	int   ii = 0;
-	bool inSer = false;
-	bool inLoc = false;
-	int j = 0;
+void contexts_count(std::vector<servers> &srvs, std::string path) {
+  std::string buff;
+  std::ifstream file(path);
+  std::string word;
+  std::vector<std::string> serv_dirs;
+  int i = 0;
+  int ii = 0;
+  bool inSer = false;
+  bool inLoc = false;
+  int j = 0;
 
-	dirs(serv_dirs);
-	if (file.fail())
-		ft_perr("Error: Bad file permissions, or file doesn't exist!");
-	while (getline(file, buff))
-	{
-		if (buff.length())
-		{
-			buff.erase(0, buff.find_first_not_of(" \t"));
+  dirs(serv_dirs);
+  if (file.fail())
+    ft_perr("Error: Bad file permissions, or file doesn't exist!");
+  while (getline(file, buff)) {
+    if (buff.length()) {
+      buff.erase(0, buff.find_first_not_of(" \t"));
       if (buff[0] == '#')
-        continue ;
-			j = buff.length() - 1;
-			while (j > 0 && (buff[j] == ' ' || buff[j] == '\t'))
-				j--;
-			buff.erase(j+1, buff.length());
+        continue;
+      j = buff.length() - 1;
+      while (j > 0 && (buff[j] == ' ' || buff[j] == '\t'))
+        j--;
+      buff.erase(j + 1, buff.length());
 
-			if (!inSer && (!buff.compare(0, 6,"server") && buff.length() > 6))
-				ft_perr("Error: bad syntax!");
+      if (!inSer && (!buff.compare(0, 6, "server") && buff.length() > 6))
+        ft_perr("Error: bad syntax!");
 
-			if(!buff.compare("server") && !inSer)
-			{
-				srvs.push_back(servers());
-				getline(file, buff);
-				buff.erase(std::remove_if(buff.begin(), buff.end(), isspace), buff.end());
-				if (buff.compare("{") != 0) 
-					ft_perr("Error: Opening bracket is missing !");
-				inSer = true;
-				i++;
-			}
-			else if(!buff.compare("server") && inSer)
-				ft_perr("Error: Server can't exist within a context");
+      if (!buff.compare("server") && !inSer) {
+        srvs.push_back(servers());
+        getline(file, buff);
+        buff.erase(std::remove_if(buff.begin(), buff.end(), isspace),
+                   buff.end());
+        if (buff.compare("{") != 0)
+          ft_perr("Error: Opening bracket is missing !");
+        inSer = true;
+        i++;
+      } else if (!buff.compare("server") && inSer)
+        ft_perr("Error: Server can't exist within a context");
 
-			if(inSer && (buff.compare(0, 8,"location") == 0 && buff.length() == 8))
-			{
-				srvs[i-1].loc.push_back(locations());
+      if (inSer &&
+          (buff.compare(0, 8, "location") == 0 && buff.length() == 8)) {
+        srvs[i - 1].loc.push_back(locations());
         ii++;
 
-				getline(file, buff);
-				buff.erase(std::remove_if(buff.begin(), buff.end(), isspace), buff.end());
-				if (buff.compare("{") != 0) 
-					ft_perr("Error: Opening bracket is missing for location!");
-				inLoc = true;
-			}
-			else if(inSer && (!buff.compare(0, 8,"location") && buff.length() != 8))
-				ft_perr("Error: bad syntax!");
-			if (inLoc && !buff.compare("}"))
-				inLoc = false;
+        getline(file, buff);
+        buff.erase(std::remove_if(buff.begin(), buff.end(), isspace),
+                   buff.end());
+        if (buff.compare("{") != 0)
+          ft_perr("Error: Opening bracket is missing for location!");
+        inLoc = true;
+      } else if (inSer &&
+                 (!buff.compare(0, 8, "location") && buff.length() != 8))
+        ft_perr("Error: bad syntax!");
+      if (inLoc && !buff.compare("}"))
+        inLoc = false;
 
-			else if (inSer && !buff.compare("}"))
-				inSer = false;
+      else if (inSer && !buff.compare("}"))
+        inSer = false;
 
-			else if (!directive(buff, serv_dirs, srvs[i-1], inLoc, ii))
-				ft_perr("Error: Uknown directive!");
-		}
-	}
-	if (inSer || inLoc)
-		ft_perr("Error: missing Bracket!");
+      else if (!directive(buff, serv_dirs, srvs[i - 1], inLoc, ii - 1))
+        ft_perr("Error: Uknown directive!");
+    }
+  }
+  if (inSer || inLoc)
+    ft_perr("Error: missing Bracket!");
 }
 
-int main(int ac, char **av)
-{
-	std::string path;
+int main(int ac, char **av) {
+  std::string path;
 
-	if (ac >= 3)
-		ft_perr("Error!\nusage: ./webserv [configuration file]\n");
-	else if (ac == 1) 
-		path = "default.conf";
-	else if (ac == 2)
-		path = av[1];
+  if (ac >= 3)
+    ft_perr("Error!\nusage: ./webserv [configuration file]\n");
+  else if (ac == 1)
+    path = "default.conf";
+  else if (ac == 2)
+    path = av[1];
 
-	std::vector<servers> servs;
-	contexts_count(servs, path);
+  std::vector<servers> servs;
+  contexts_count(servs, path);
 
-	// std::cout << "The length of serv: " << servs.size() << std::endl;
-	// std::cout << "The length of locs: " << servs[0].loc.size() << std::endl;
-	// std::cout << "we have stored in serv " << servs[0].port << "." << std::endl;
-	// std::cout << "we have stored in serv " << servs[0].server_name <<"." << std::endl;
-
-
+  std::cout << "The length of serv: " << servs.size() << std::endl;
+  std::cout << "The length of locs: " << servs[0].loc.size() << std::endl;
+  // std::cout << "we have stored in serv " << servs[0].port << "." <<
+  // std::endl; std::cout << "we have stored in serv " << servs[0].server_name
+  // <<"." << std::endl;
 }
--- parsing/parsing.hpp
+++ parsing/parsing.hpp
@@ -1,45 +1,41 @@
 #ifndef PARSING_HPP
-#define  PARSING_HPP
+#define PARSING_HPP
 
-#include <string>
 #include <algorithm>
+#include <cctype>
 #include <cstdlib>
-#include <iostream>
 #include <fstream>
 #include <sstream>
-#include  <cctype> 
+#include <string>
 #include <vector>
 
-
-class locations
-{
-  private:
-
-  public:
-    std::string pattern;
-    std::string methods;
-    std::string redir_path;
-    std::string root;
-    bool        autoindex;
-    std::string err_page;
-    std::vector<std::pair<std::string, std::string> > cgi; //vec.push_back(make_pair(string, string2);
-    locations();
-    ~locations();
+class locations {
+private:
+public:
+  std::string pattern;
+  std::string methods;
+  std::string redir_path;
+  std::string root;
+  bool autoindex;
+  std::vector<std::string> def;
+  std::vector<std::pair<std::string, std::string>>
+      cgi; // vec.push_back(make_pair(string, string2);
+  locations();
+  ~locations();
 };
 
-class servers
-{
-  private:
-
-  public:
-    std::vector <locations> loc;
-    std::string  port;
-    std::string   host;
-    std::string   server_name;
-    //int client_max_body_size;
-    std::string client_max_body_size;
-    servers();
-    ~servers();
+class servers {
+private:
+public:
+  std::vector<locations> loc;
+  std::string port;
+  std::string host;
+  std::string server_name;
+  // int client_max_body_size;
+  std::string client_max_body_size;
+  // error pages are missing here;
+  servers();
+  ~servers();
 };
 
 #endif
